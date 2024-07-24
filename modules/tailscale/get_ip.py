d23e989746840758ef704ca0a97ca3cd312ce90a
CYTMWIA <58131156+CYTMWIA@users.noreply.github.com>
2024-07-25 01:54:22 +0800

    获取 tailscale ip

---
+++ modules/tailscale/get_ip.py
@@ -0,0 +1,17 @@
+from modules.core import Host, auto_host
+
+
+@auto_host
+def get_ip(host: Host):
+    res = host.run("ip --brief address show")
+    if res.failed:
+        raise Exception(str(res))
+
+    lines = res.stdout.splitlines()
+    for line in lines:
+        if line.startswith("tailscale"):
+            ip_with_mask = line.split()[2]
+            ip_only = ip_with_mask[: ip_with_mask.index("/")]
+            return ip_only
+
+    raise Exception("Could not find tailscale ip")
