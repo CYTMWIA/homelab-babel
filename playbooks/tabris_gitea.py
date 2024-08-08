2fd02e75fb1737661c43eae14650061d8050b240
CYTMWIA <58131156+CYTMWIA@users.noreply.github.com>
2024-08-09 01:48:10 +0800

    更新 tabris_gitea

--- playbooks/tabris_gitea.py
+++ playbooks/tabris_gitea.py
@@ -3,4 +3,7 @@ from iapyc.core import host, Host
 
 h: Host = host("tabris")
 with h:
-    gitea.setup(base_dir="/srv/gitea")
+    gitea.setup(
+        base_dir="/srv/gitea",
+        http_proxy="http://192.168.31.103:7890",
+    )
